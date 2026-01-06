def28c0fc961984ba77eb5c6a1a4badd132bafab
Dewan Sajid Islam <111456935+Dewan-Sajid-Islam@users.noreply.github.com>
2026-01-07 04:14:28 +0600

    init.py

---
+++ scripts/init.py
@@ -0,0 +1,13 @@
+"""
+Entanglement Dark Energy (EDE) Model
+Author: Dewan Sajid Islam
+Date: 2026-07-01
+
+This package contains all the scripts for reproducing the results of the paper:
+"The Entropic Genesis of Cosmic Acceleration: Quantum-Gravitational Entanglement 
+Entropy as the Dynamical Origin of Dark Energy"
+"""
+
+__version__ = "1.0.0"
+__author__ = "Dewan Sajid Islam"
+__email__ = "johnnykitty06@gmail.com"
